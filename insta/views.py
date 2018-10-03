aa8f4bdc0159a6a3d289dd9763a470b120459c41
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-03 15:30:18 +0300

    added view function to display images on homepage

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
 def index(request):
   '''
   View function that renders the index page
   '''
-  return render(request, 'index.html')
+  pictures = Image.get_all_images()
+  return render(request, 'index.html',{"pictures":pictures})
+
+# def images(request):
+#   '''
+#   View function that queries the database and returns images added
+#   '''
+#   pictures = Image.get_all_images()
+
+#   return render(request,'index.html',{"pictures":pictures})
