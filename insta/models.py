0bfe5d2608036e2d8b7649ac7fc74f4a6b5cce87
Gift-Lumumba <giftlumumba2@gmail.com>
2018-10-03 12:48:37 +0300

    created Image and Profile class models and their properties and methods

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  profile_pic = models.ImageField(upload_to='images/')
+  name = models.CharField(max_length=50)
+  bio = models.TextField()
+
+  def __str__(self):
+    return self.name
+
+  def save_profile(self):
+    self.save()
+
+  def update_profile(self):
+    self.update()
+
+  def delete_profile(self):
+    self.delete()
+
+  @classmethod
+  def search_user(cls, name):
+      info = cls.objects.filter(profile__name__icontains=name)
+      return info
+
+class Image(models.Model):
+  name = models.CharField(max_length=50)
+  image = models.ImageField(upload_to = 'images/')
+  caption = models.TextField()
+  profile = models.ForeignKey(Profile)
+
+  def __str__(self):
+      return self.name
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
+
+  def update_caption(self):
+    self.update()
+
+
+
+
+
+
